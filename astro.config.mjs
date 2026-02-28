3b0c158498ebc8bf5300f56072628e236e3e495a
Matthew Strickland <matthew@idlefusion.com>
2026-02-28 12:21:27 -0500

    fix: add sitemap integration via @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://idlefusion.com',
   base: '/',
   output: 'static',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   build: {
     assets: '_assets'
   }
